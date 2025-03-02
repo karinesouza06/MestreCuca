c8b0c129cbd6a9d0f6992c55ad80187791d59465
Riversson Paulo <riverssonp.santos@gmail.com>
2025-03-02 00:06:37 -0300

    Update recipe_controller.py

--- MestreCuca/controllers/recipe_controller.py
+++ MestreCuca/controllers/recipe_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, url_for, flash, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.recipe import Receita
-from models.user import User  
+from models.user import User, current_user
 from database.db import db_session
 
 recipe_bp = Blueprint('recipes', __name__, url_prefix='/recipes')
@@ -23,14 +23,14 @@ def cadastrar_receitas():
                 flash("Usuário não encontrado.")
                 return redirect(url_for('recipes.cadastrar_receitas'))
 
-            receita = Receita(nome_receita=nome_receita, nome_user=user_id, categoria=categoria, ingredientes=ingredientes, modo_preparo=modo_preparo)
+            receita = Receita(nome_receita=nome_receita, nome_user=nome_user, categoria=categoria, ingredientes=ingredientes, modo_preparo=modo_preparo)
             db_session.add(receita)
             db_session.commit()
-            return render_template('pagina_receitas.html')
-    return render_template('cadastrar_receitas.html')
+            return render_template('cadastrar_receitas.html', user=current_user)
+    return render_template('cadastrar_receitas.html', user=current_user)
 
 @recipe_bp.route('/pagina_receitas', methods=['GET'])
-def listar_todas_receitas():
+def pagina_receitas():
     receitas = Receita.get_receitas()
     return render_template('pagina_receitas.html', receitas=receitas)
 
@@ -44,10 +44,14 @@ def editar_receita():
         modo_preparo = request.form['modo_preparo']
         
         Receita.update(nome_user, nome_receita, categoria, ingredientes, modo_preparo)
-        return redirect(url_for('recepies.listar_todas_receitas'))
+        return redirect(url_for('recepies.pagina_receitas'))
         
 @recipe_bp.route('/excluir_receita/<int:id>', methods=['GET'])
 def excluir_receitas(id):   
        Receita.delete(id)
-       return redirect(url_for('recepies.listar_todas_receitas'))
+       return redirect(url_for('recepies.pagina_receitas'))
+
+
+    
+   
     
